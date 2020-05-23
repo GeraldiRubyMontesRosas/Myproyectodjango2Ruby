c0da83c19dc56f31d280336d53c2a45c2a1935ec
rubym <ruby.montes.rosas.gmail.com>
2020-05-22 23:47:30 -0500

    se realizo cambio catorce

--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, 
         verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, 
@@ -12,7 +13,7 @@ class Page(models.Model):
     class Meta:
         verbose_name = "pagina"
         verbose_name_plural = "paginas"
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
